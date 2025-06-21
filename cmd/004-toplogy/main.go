095b5ce90dee72db9b8bf248965e201db4064f4e
dane <dane@medieval.software>
2025-06-21 19:46:58 -0400

    inw: software

--- cmd/004-toplogy/main.go
+++ cmd/004-toplogy/main.go
@@ -16,8 +16,11 @@ import (
 	"os"
 	"runtime"
 	"runtime/pprof"
-	"slices"
 	"time"
+	"unsafe"
+
+	"net/http"
+	_ "net/http/pprof"
 
 	"github.com/go-gl/mathgl/mgl32"
 	"github.com/hajimehoshi/ebiten/v2"
@@ -37,26 +40,23 @@ type (
 	vec3  = mgl32.Vec3
 	vec4  = mgl32.Vec4
 	mat4  = mgl32.Mat4
+	quat  = mgl32.Quat
 )
 
 var shader = `
 //kage:unit pixels
 package main
 
-var HoverID int
-
 func Fragment(dst vec4, src vec2, rgba vec4, custom vec4) vec4 {
 	src_origin := imageSrc0Origin()
 
-	if int(custom.r*65535.0) == HoverID {
-		return vec4(1, 0, 0, 1)
-	}
-	
 	// atlas -> texture space
 	texel := src - src_origin
 
-	// perspective divide (W is stored in rgba.a)
-	texel /= rgba.a
+	// perspective divide
+	if custom.w != 0.0 {
+		texel /= custom.w
+	}
 
 	// scale uv to pixels
 	texel *= imageSrc0Size()
@@ -107,6 +107,10 @@ func main() {
 			}
 		}()
 	}
+
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
 
 	shader, err := ebiten.NewShader([]byte(shader))
 
@@ -192,7 +196,7 @@ type game struct {
 	context            *context
 	cycle              float32
 	texture            *ebiten.Image
-	mesh               *mesh
+	mesh               *mesh_t
 	topology_mask      [32 * 32]byte
 	topology_radius    float64
 	topology_curvature float64
@@ -218,19 +222,19 @@ type camera struct {
 }
 
 type triangle struct {
-	p1, p2, p3 uint16
+	v1, v2, v3 uint16
 	t1, t2, t3 uint16
 }
 
-type mesh struct {
+type mesh_t struct {
 	triangles []triangle
 	points    []vec3
-	texcoords []vec2
+	uvs       []vec2
 }
 
 type vertex struct {
-	position vec4
-	texcoord vec2
+	pos vec4
+	uv  vec2
 }
 
 func interpolate_vec4(v1, v2, v3 vec4, f vec3) (result vec4) {
@@ -248,71 +252,71 @@ func interpolate_vec2(v1, v2, v3 vec2, f vec3) (result vec2) {
 }
 
 func interpolate_vertex(v1, v2, v3 vertex, f vec3) (result vertex) {
-	result.position = interpolate_vec4(v1.position, v2.position, v3.position, f)
-	result.texcoord = interpolate_vec2(v1.texcoord, v2.texcoord, v3.texcoord, f)
+	result.pos = interpolate_vec4(v1.pos, v2.pos, v3.pos, f)
+	result.uv = interpolate_vec2(v1.uv, v2.uv, v3.uv, f)
 	return
 }
 
 type viewport struct {
-	x   int
-	y   int
-	w   int
-	h   int
-	w_2 int
-	h_2 int
+	x      int
+	y      int
+	w      int
+	h      int
+	w_half int
+	h_half int
 }
 
-func (self *game) Layout(outerWidth, outerHeight int) (int, int) {
+func (g *game) Layout(outerWidth, outerHeight int) (int, int) {
 	return game_width, game_height
 }
 
-func (self *game) Update() error {
-	self.cycle++
+func (g *game) Update() error {
+	g.cycle++
 
 	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
 		cx, cy := ebiten.CursorPosition()
 
 		// doing the logic in the next update ensures we don't get some crazy snapping
-		if !self.camera.dragging {
-			self.camera.dragging = true
+		if !g.camera.dragging {
+			g.camera.dragging = true
 		} else {
-			dx := float(cx-self.camera.drag_x) / 100.0
-			dy := float(cy-self.camera.drag_y) / 100.0
+			dx := float(cx-g.camera.drag_x) / 100.0
+			dy := float(cy-g.camera.drag_y) / 100.0
 
-			self.camera.pitch = mgl32.Clamp(self.camera.pitch+dy, -math.Pi/2, math.Pi/2)
-			self.camera.yaw -= dx
+			g.camera.pitch = mgl32.Clamp(g.camera.pitch+dy, -math.Pi/2, math.Pi/2)
+			g.camera.yaw -= dx
 
 			view := mgl32.Ident4()
-			view = view.Mul4(mgl32.HomogRotate3DX(self.camera.pitch))
-			view = view.Mul4(mgl32.HomogRotate3DY(self.camera.yaw))
+			view = view.Mul4(mgl32.HomogRotate3DX(g.camera.pitch))
+			view = view.Mul4(mgl32.HomogRotate3DY(g.camera.yaw))
 
-			self.camera.right = view.Row(0).Vec3().Mul(-1)
-			self.camera.up = view.Row(1).Vec3()
-			self.camera.forward = view.Row(2).Vec3().Mul(-1)
+			g.camera.right = view.Row(0).Vec3().Mul(-1)
+			g.camera.up = view.Row(1).Vec3()
+			g.camera.forward = view.Row(2).Vec3().Mul(-1)
 
 			if ebiten.IsKeyPressed(ebiten.KeyW) || ebiten.IsKeyPressed(ebiten.KeyUp) {
-				self.camera.pos = self.camera.pos.Add(self.camera.forward.Mul(0.1))
+				g.camera.pos = g.camera.pos.Add(g.camera.forward.Mul(0.1))
 			} else if ebiten.IsKeyPressed(ebiten.KeyS) || ebiten.IsKeyPressed(ebiten.KeyDown) {
-				self.camera.pos = self.camera.pos.Sub(self.camera.forward.Mul(0.1))
+				g.camera.pos = g.camera.pos.Sub(g.camera.forward.Mul(0.1))
 			}
 
 			if ebiten.IsKeyPressed(ebiten.KeyD) || ebiten.IsKeyPressed(ebiten.KeyRight) {
-				self.camera.pos = self.camera.pos.Add(self.camera.right.Mul(0.1))
+				g.camera.pos = g.camera.pos.Add(g.camera.right.Mul(0.1))
 			} else if ebiten.IsKeyPressed(ebiten.KeyA) || ebiten.IsKeyPressed(ebiten.KeyLeft) {
-				self.camera.pos = self.camera.pos.Sub(self.camera.right.Mul(0.1))
+				g.camera.pos = g.camera.pos.Sub(g.camera.right.Mul(0.1))
 			}
 
-			self.camera.view_matrix = view.Mul4(mgl32.Translate3D(
-				-self.camera.pos.X(),
-				-self.camera.pos.Y(),
-				-self.camera.pos.Z(),
+			g.camera.view_matrix = view.Mul4(mgl32.Translate3D(
+				-g.camera.pos.X(),
+				-g.camera.pos.Y(),
+				-g.camera.pos.Z(),
 			))
 		}
 
-		self.camera.drag_x = cx
-		self.camera.drag_y = cy
+		g.camera.drag_x = cx
+		g.camera.drag_y = cy
 	} else {
-		self.camera.dragging = false
+		g.camera.dragging = false
 	}
 
 	return nil
@@ -410,12 +414,20 @@ type context struct {
 	clip_space_points []vec4
 	vertices          []ebiten.Vertex
 	indices           []uint16
+
+	use_cpu        bool
+	cpu_buffer     *ebiten.Image
+	cpu_pixels     []uint32
+	cpu_pixels_raw []byte
+}
+
+func init() {
+
 }
 
 type screen_triangle struct {
 	v1, v2, v3 vertex
 	data       uint64
-	distance   float
 }
 
 func (c *context) set_viewport(x, y, w, h int) {
@@ -423,76 +435,187 @@ func (c *context) set_viewport(x, y, w, h int) {
 	c.viewport.y = y
 	c.viewport.w = w
 	c.viewport.h = h
-	c.viewport.w_2 = w / 2
-	c.viewport.h_2 = h / 2
+	c.viewport.w_half = w / 2
+	c.viewport.h_half = h / 2
 }
 
-func clip_out_of_bounds(a vec4) bool {
+func out_of_bounds(a vec4) bool {
 	x, y, z, w := a.X(), a.Y(), a.Z(), a.W()
 	return x < -w || x > w || y < -w || y > w || z < -w || z > w
 }
 
-func (c *context) clip_to_ndc(src vec4) (ndc vec4) {
-	inv_w := 1.0 / src.W()
-	ndc = vec4{
-		src.X() * inv_w,
-		src.Y() * inv_w,
-		src.Z() * inv_w,
-		src.W(), //  retain W for later
-	}
-	return
+func viewport_transform(ndc, dimension_half float) float {
+	return dimension_half*ndc + dimension_half
 }
 
-func (c *context) ndc_to_screen(src vec4) vec4 {
-	w_2 := float(c.viewport.w_2)
-	h_2 := float(c.viewport.h_2)
-	return vec4{
-		w_2*src.X() + w_2,
-		h_2*src.Y() + h_2,
-		src.Z(),
-		src.W(),
-	}
-}
+func (ctx *context) push_mesh(mesh *mesh_t, position vec3, orientation quat, mesh_data uint64) int {
+	// TODO: determine if mesh is culled
 
-func (ctx *context) draw_mesh(mesh *mesh, mesh_data uint64, texture, target *ebiten.Image) int {
 	// save us some calculations by doing this here instead of per point
-	projection_view_matrix := ctx.proj_matrix.Mul4(ctx.view_matrix)
+	projection_view_matrix := ctx.proj_matrix.Mul4(
+		ctx.view_matrix.Mul4(
+			orientation.Mat4().Mul4(
+				mgl32.Translate3D(
+					position.X(),
+					position.Y(),
+					position.Z(),
+				),
+			),
+		),
+	)
 
 	// transform all the mesh points into clip space
 	for _, point := range mesh.points {
+		// TODO: perform local transformations here (skeleton?)
+
 		point := projection_view_matrix.Mul4x1(point.Vec4(1))
 		ctx.clip_space_points = append(ctx.clip_space_points, point)
 	}
 
-	mesh_data <<= 16
-
-	for t, triangle := range mesh.triangles {
-		buf := ctx.triangle_buffer[:0]
-
+	for _, triangle := range mesh.triangles {
 		v1 := vertex{
-			position: ctx.clip_space_points[triangle.p1],
-			texcoord: mesh.texcoords[triangle.t1],
+			pos: ctx.clip_space_points[triangle.v1],
+			uv:  mesh.uvs[triangle.t1],
 		}
 		v2 := vertex{
-			position: ctx.clip_space_points[triangle.p2],
-			texcoord: mesh.texcoords[triangle.t2],
+			pos: ctx.clip_space_points[triangle.v2],
+			uv:  mesh.uvs[triangle.t2],
 		}
 		v3 := vertex{
-			position: ctx.clip_space_points[triangle.p3],
-			texcoord: mesh.texcoords[triangle.t3],
+			pos: ctx.clip_space_points[triangle.v3],
+			uv:  mesh.uvs[triangle.t3],
 		}
 
-		if clip_out_of_bounds(v1.position) || clip_out_of_bounds(v2.position) || clip_out_of_bounds(v3.position) {
-			buf = ctx.push_triangle_clipped(buf, v1, v2, v3, mesh_data|uint64(t))
+		if out_of_bounds(v1.pos) || out_of_bounds(v2.pos) || out_of_bounds(v3.pos) {
+			points := sutherland_hodgman_3d(v1.pos, v2.pos, v3.pos)
+
+			p1 := v1.pos.Vec3()
+			p2 := v2.pos.Vec3()
+			p3 := v3.pos.Vec3()
+
+			for i := 2; i < len(points); i++ {
+				b1 := barycentric(p1, p2, p3, points[0].Vec3())
+				b2 := barycentric(p1, p2, p3, points[i-1].Vec3())
+				b3 := barycentric(p1, p2, p3, points[i].Vec3())
+
+				ctx.triangle_buffer = append(ctx.triangle_buffer, screen_triangle{
+					v1:   interpolate_vertex(v1, v2, v3, b1),
+					v2:   interpolate_vertex(v1, v2, v3, b2),
+					v3:   interpolate_vertex(v1, v2, v3, b3),
+					data: mesh_data,
+				})
+			}
 		} else {
-			buf = ctx.push_triangle(buf, v1, v2, v3, mesh_data|uint64(t))
+			ctx.triangle_buffer = append(ctx.triangle_buffer, screen_triangle{
+				v1:   v1,
+				v2:   v2,
+				v3:   v3,
+				data: mesh_data,
+			})
 		}
 
-		if len(buf) > 0 {
-			ctx.push_triangles_ebiten(buf, texture, target)
+		w_half := float(ctx.viewport.w_half)
+		h_half := float(ctx.viewport.h_half)
+
+		var r1 ebiten.Vertex
+		var r2 ebiten.Vertex
+		var r3 ebiten.Vertex
+
+		for _, t := range ctx.triangle_buffer {
+			v1 := t.v1
+			v2 := t.v2
+			v3 := t.v3
+
+			// perspective divide (clip -> ndc)
+			inv_w1 := 1.0 / v1.pos.W()
+			inv_w2 := 1.0 / v2.pos.W()
+			inv_w3 := 1.0 / v3.pos.W()
+
+			r1.DstX = v1.pos.X() * inv_w1
+			r1.DstY = v1.pos.Y() * inv_w1
+			r2.DstX = v2.pos.X() * inv_w2
+			r2.DstY = v2.pos.Y() * inv_w2
+			r3.DstX = v3.pos.X() * inv_w3
+			r3.DstY = v3.pos.Y() * inv_w3
+
+			// 2d cross product
+			dx12 := (r2.DstX - r1.DstX)
+			dy12 := (r2.DstY - r1.DstY)
+			dx13 := (r3.DstX - r1.DstX)
+			dy13 := (r3.DstY - r1.DstY)
+
+			// back-face culling
+			if dx12*dy13-dx13*dy12 <= 0 {
+				continue
+			}
+
+			// ndc to screen space
+			r1.DstX = viewport_transform(r1.DstX, w_half)
+			r1.DstY = viewport_transform(r1.DstY, h_half)
+			r2.DstX = viewport_transform(r2.DstX, w_half)
+			r2.DstY = viewport_transform(r2.DstY, h_half)
+			r3.DstX = viewport_transform(r3.DstX, w_half)
+			r3.DstY = viewport_transform(r3.DstY, h_half)
+
+			// perspective correction
+			if true {
+				r1.SrcX = v1.uv.X() * inv_w1
+				r1.SrcY = v1.uv.Y() * inv_w1
+				r1.Custom3 = inv_w1
+
+				r2.SrcX = v2.uv.X() * inv_w2
+				r2.SrcY = v2.uv.Y() * inv_w2
+				r2.Custom3 = inv_w2
+
+				r3.SrcX = v3.uv.X() * inv_w3
+				r3.SrcY = v3.uv.Y() * inv_w3
+				r3.Custom3 = inv_w3
+			} else {
+				r1.SrcX = v1.uv.X()
+				r1.SrcY = v1.uv.Y()
+				r2.SrcX = v2.uv.X()
+				r2.SrcY = v2.uv.Y()
+				r3.SrcX = v3.uv.X()
+				r3.SrcY = v3.uv.Y()
+			}
+
+			ctx.vertices = append(ctx.vertices, r1, r2, r3)
+
+			// gpu needs an index buffer
+			if !ctx.use_cpu {
+				first_index := uint16(len(ctx.indices))
+				ctx.indices = append(ctx.indices, first_index, first_index+1, first_index+2)
+			}
 		}
+
+		ctx.triangle_buffer = ctx.triangle_buffer[:0]
 	}
 
+	ctx.clip_space_points = ctx.clip_space_points[:0]
+
+	return 0
+}
+
+func (ctx *context) draw(texture, target *ebiten.Image) {
+	if ctx.use_cpu {
+		ctx.draw_cpu(texture, target)
+	} else {
+		ctx.draw_gpu(texture, target)
+	}
+}
+
+func (ctx *context) draw_cpu(texture, target *ebiten.Image) {
+	dst_bounds := target.Bounds()
+	dst_size := dst_bounds.Dx() * dst_bounds.Dy()
+	if ctx.cpu_pixels == nil || len(ctx.cpu_pixels) != dst_size {
+		ctx.cpu_pixels = make([]uint32, dst_bounds.Dx()*dst_bounds.Dy())
+		ctx.cpu_pixels_raw = unsafe.Slice((*byte)(unsafe.Pointer(unsafe.SliceData(ctx.cpu_pixels))), len(ctx.cpu_pixels)*4)
+	}
+
+	target.WritePixels(ctx.cpu_pixels_raw)
+}
+
+func (ctx *context) draw_gpu(texture, target *ebiten.Image) {
 	target.DrawTrianglesShader(ctx.vertices, ctx.indices, ctx.shader, &ebiten.DrawTrianglesShaderOptions{
 		Images: [4]*ebiten.Image{
 			texture,
@@ -504,170 +627,21 @@ func (ctx *context) draw_mesh(mesh *mesh, mesh_data uint64, texture, target *ebi
 	})
 
 	ctx.drawn_triangles = len(ctx.indices) / 3
-
-	// reset buffers
-	ctx.clip_space_points = ctx.clip_space_points[:0]
 	ctx.vertices = ctx.vertices[:0]
 	ctx.indices = ctx.indices[:0]
-
-	return 0
 }
 
-var highest int
-
-func (c *context) push_triangle_clipped(dst []screen_triangle, v1, v2, v3 vertex, bitset uint64) []screen_triangle {
-	points := sutherland_hodgman_3d(v1.position, v2.position, v3.position)
-
-	p1 := v1.position.Vec3()
-	p2 := v2.position.Vec3()
-	p3 := v3.position.Vec3()
-
-	if len(points) > highest {
-		highest = len(points)
-		println(highest)
-	}
-
-	for i := 2; i < len(points); i++ {
-		b1 := barycentric(p1, p2, p3, points[0].Vec3())
-		b2 := barycentric(p1, p2, p3, points[i-1].Vec3())
-		b3 := barycentric(p1, p2, p3, points[i].Vec3())
-
-		dst = c.push_triangle(
-			dst,
-			interpolate_vertex(v1, v2, v3, b1),
-			interpolate_vertex(v1, v2, v3, b2),
-			interpolate_vertex(v1, v2, v3, b3),
-			bitset,
-		)
-	}
-	return dst
-}
-
-func (c *context) project_triangle(v1, v2, v3 vertex, data uint64) (tri screen_triangle, ok bool) {
-	ndc1 := c.clip_to_ndc(v1.position)
-	ndc2 := c.clip_to_ndc(v2.position)
-	ndc3 := c.clip_to_ndc(v3.position)
-
-	// back-face culling
-	if (ndc2.X()-ndc1.X())*(ndc3.Y()-ndc1.Y())-(ndc3.X()-ndc1.X())*(ndc2.Y()-ndc1.Y()) <= 0 {
-		return
-	}
-
-	v1.position = c.ndc_to_screen(ndc1)
-	v2.position = c.ndc_to_screen(ndc2)
-	v3.position = c.ndc_to_screen(ndc3)
-
-	tri.v1 = v1
-	tri.v2 = v2
-	tri.v3 = v3
-	tri.data = data
-	tri.distance = (v1.position.Z() + v2.position.Z() + v3.position.Z()) / 3
-
-	return tri, true
-}
-
-func (c *context) push_triangle(dst []screen_triangle, v1, v2, v3 vertex, data uint64) []screen_triangle {
-	if tri, ok := c.project_triangle(v1, v2, v3, data); ok {
-		return append(dst, tri)
-	}
-	return dst
-}
-
-func (ctx *context) sort_triangles(triangles []screen_triangle) {
-	slices.SortFunc(triangles, func(a, b screen_triangle) int {
-		if a.distance >= b.distance {
-			return -1
-		}
-		return 1
-	})
-}
-
-func (ctx *context) push_triangles_ebiten(triangles []screen_triangle, texture, target *ebiten.Image) (hover_id int) {
-	cx, cy := ebiten.CursorPosition()
-	check_input := true
-
-	const inv_custom = 1.0 / 65535.0
-
-	for _, triangle := range triangles {
-		v1 := triangle.v1
-		v2 := triangle.v2
-		v3 := triangle.v3
-
-		if check_input {
-			y1 := v1.position.Y()
-			y2 := v2.position.Y()
-			y3 := v3.position.Y()
-			x1 := v1.position.X()
-			x2 := v2.position.X()
-			x3 := v3.position.X()
-
-			// if point_in_triangle_bounds(float32(cx), float32(cy),x1, y1, x2, y2, x3, y3) {
-			if point_in_triangle(float32(cx), float32(cy), x1, y1, x2, y2, x3, y3) {
-				hover_id = int(triangle.data & 0xFFFF)
-				check_input = false
-			}
-		}
-
-		inv_w1 := 1.0 / v1.position.W()
-		inv_w2 := 1.0 / v2.position.W()
-		inv_w3 := 1.0 / v3.position.W()
-
-		custom0 := float32(triangle.data&0xFFFF) / 65535.0
-
-		ctx.vertices = append(ctx.vertices,
-			ebiten.Vertex{
-				SrcX:    v1.texcoord.X() * inv_w1,
-				SrcY:    v1.texcoord.Y() * inv_w1,
-				DstX:    v1.position.X(),
-				DstY:    v1.position.Y(),
-				ColorR:  1,
-				ColorG:  1,
-				ColorB:  1,
-				ColorA:  inv_w1,
-				Custom0: custom0,
-			},
-			ebiten.Vertex{
-				SrcX:    v2.texcoord.X() * inv_w2,
-				SrcY:    v2.texcoord.Y() * inv_w2,
-				DstX:    v2.position.X(),
-				DstY:    v2.position.Y(),
-				ColorR:  1,
-				ColorG:  1,
-				ColorB:  1,
-				ColorA:  inv_w2,
-				Custom0: custom0,
-			},
-			ebiten.Vertex{
-				SrcX:    v3.texcoord.X() * inv_w3,
-				SrcY:    v3.texcoord.Y() * inv_w3,
-				DstX:    v3.position.X(),
-				DstY:    v3.position.Y(),
-				ColorR:  1,
-				ColorG:  1,
-				ColorB:  1,
-				ColorA:  inv_w3,
-				Custom0: custom0,
-			},
-		)
-
-		first_index := uint16(len(ctx.indices))
-		ctx.indices = append(ctx.indices, first_index, first_index+1, first_index+2)
-	}
-
-	return
-}
-
-func (self *game) Draw(screen *ebiten.Image) {
+func (g *game) Draw(screen *ebiten.Image) {
 	defer func(t time.Time) {
 		ft := time.Now().Sub(t)
-		if self.frametime == 0 {
-			self.frametime = ft
+		if g.frametime == 0 {
+			g.frametime = ft
 		} else {
-			self.frametime += (ft - self.frametime) / 2
+			g.frametime += (ft - g.frametime) / 2
 		}
 	}(time.Now())
 
-	ctx := self.context
+	ctx := g.context
 
 	w := screen.Bounds().Dx()
 	h := screen.Bounds().Dy()
@@ -681,30 +655,44 @@ func (self *game) Draw(screen *ebiten.Image) {
 	ctx.proj_matrix = mgl32.Perspective(30, game_aspect, 0.1, 100)
 
 	// the camera view matrix is invalid until the user controls it
-	if self.camera.view_matrix.Det() == 0 {
+	if g.camera.view_matrix.Det() == 0 {
 		ctx.view_matrix = mgl32.LookAtV(
 			vec3{0, 7, 19},
 			vec3{0, 0, 0},
 			vec3{0, 1, 0},
 		)
 	} else {
-		ctx.view_matrix = self.camera.view_matrix
+		ctx.view_matrix = g.camera.view_matrix
 	}
 
 	screen.Fill(color.RGBA{130, 130, 130, 255})
-	hover_id := ctx.draw_mesh(self.mesh, 0, self.texture, screen)
+
+	// position := vec3{0, 0, 0}
+
+	orientation := mgl32.QuatIdent()
+	angle := g.cycle / 60 / math.Pi
+	orientation = orientation.Mul(mgl32.QuatRotate(angle, vec3{0, 1, 0}))
+
+	// hover_id := ctx.push_mesh(g.mesh, position, orientation, 0)
+
+	ctx.draw(g.texture, screen)
+
+	var mem runtime.MemStats
+	runtime.ReadMemStats(&mem)
 
 	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("TPS: %.0f", ebiten.ActualTPS()), 0, 0)
-	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("FPS: %.0f (%v)", ebiten.ActualFPS(), self.frametime), 0, 14)
-	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("Triangles: %d", ctx.drawn_triangles), 0, 28)
-	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("Eye: %.2f, %.2f", self.camera.pitch, self.camera.yaw), 0, 42)
-	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("Cam: %v", self.camera.pos), 0, 56)
-	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("HoverID: %v", hover_id), 0, 70)
+	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("FPS: %.0f CPU: %v", ebiten.ActualFPS(), g.context.use_cpu), 0, 14)
+	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("Ft: %v", g.frametime), 0, 28)
+	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("Mem: %d", mem.Alloc/1024), 0, 42)
+	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("Triangles: %d", ctx.drawn_triangles), 0, 56)
+	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("Eye: %.2f, %.2f", g.camera.pitch, g.camera.yaw), 0, 70)
+	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("Cam: %v", g.camera.pos), 0, 84)
+	// ebitenutil.DebugPrintAt(screen, fmt.Sprintf("HoverID: %v", hover_id), 0, 98)
 }
 
-func load_obj(src []byte) (*mesh, error) {
+func load_obj(src []byte) (*mesh_t, error) {
 	reader := bytes.NewReader(src)
-	mesh := &mesh{}
+	mesh := &mesh_t{}
 	for {
 		var typ string
 		if _, err := fmt.Fscan(reader, &typ); err != nil {
@@ -729,7 +717,7 @@ func load_obj(src []byte) (*mesh, error) {
 			if _, err := fmt.Fscanf(reader, "%f %f", &s, &t); err != nil {
 				return nil, fmt.Errorf("bad texcoord: %w", err)
 			}
-			mesh.texcoords = append(mesh.texcoords, vec2{s, t})
+			mesh.uvs = append(mesh.uvs, vec2{s, t})
 		case "f":
 			var v1, v2, v3 uint16
 			var t1, t2, t3 uint16
@@ -737,9 +725,9 @@ func load_obj(src []byte) (*mesh, error) {
 				return nil, fmt.Errorf("bad face: %w", err)
 			}
 			mesh.triangles = append(mesh.triangles, triangle{
-				p1: v1 - 1,
-				p2: v2 - 1,
-				p3: v3 - 1,
+				v1: v1 - 1,
+				v2: v2 - 1,
+				v3: v3 - 1,
 				t1: t1 - 1,
 				t2: t2 - 1,
 				t3: t3 - 1,
